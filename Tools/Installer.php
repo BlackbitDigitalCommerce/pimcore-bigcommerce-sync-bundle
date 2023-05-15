b8ab6effef1aada1586417b89c3e4df07302682c
Dmytro Derkach <dmytro.derkach@blackbit.de>
2023-05-15 16:31:57 +0300

    Added import objectbricks import files

--- Tools/Installer.php
+++ Tools/Installer.php
@@ -3,34 +3,37 @@
 namespace Blackbit\PimcoreBigcommerceSyncBundle\Tools;
 
 use Blackbit\DataDirectorBundle\lib\Pim\Cli;
-use Blackbit\PimcoreBigcommerceSyncBundle\Services\PimcoreService;
 use Pimcore\Extension\Bundle\Installer\AbstractInstaller;
 use Pimcore\Extension\Bundle\Installer\Exception\InstallationException;
 use Pimcore\Model\DataObject\ClassDefinition;
+use Pimcore\Model\DataObject\Objectbrick;
 use Pimcore\Model\User\Permission\Definition;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Pimcore\Model\DataObject\ClassDefinition\Service;
 
 class Installer extends AbstractInstaller
 {
-    const DEFINITION_PATH = 'Resources/definitions';
+    const DEFINITIONS_PATH = 'Resources/definitions';
+    const OBJECTBRICKS_PATH = 'Resources/objectbricks';
     const DATAPORTS_PATH = 'Resources/dataports';
 
-    private LoggerInterface $logger;
     private BundleInterface $bundle;
     private string $defenitionFullPath;
+    private string $objectbrickFullPath;
     private string $dataportFullPath;
 
     public function __construct(
         BundleInterface $bundle,
-        LoggerInterface $logger
     ) {
         parent::__construct();
         $this->defenitionFullPath = dirname(__DIR__)
             . DIRECTORY_SEPARATOR
-            . self::DEFINITION_PATH
+            . self::DEFINITIONS_PATH
+            . DIRECTORY_SEPARATOR;
+        $this->objectbrickFullPath = dirname(__DIR__)
+            . DIRECTORY_SEPARATOR
+            . self::OBJECTBRICKS_PATH
             . DIRECTORY_SEPARATOR;
         $this->dataportFullPath = dirname(__DIR__)
             . DIRECTORY_SEPARATOR
@@ -46,10 +49,16 @@ class Installer extends AbstractInstaller
 
         $result = true;
 
-        $definitionFiles = $this->getDefinitionFiles();
+        $definitionFiles = $this->getDefinitionFiles($this->defenitionFullPath, 'class');
 
         if (!count($definitionFiles)) {
             throw new InstallationException(sprintf('Bundle "%s" hasn\'t class definition templates', $this->bundle->getName()));
+        }
+
+        $objectbrickFiles = $this->getDefinitionFiles($this->objectbrickFullPath, 'objectbrick');
+
+        if (!count($objectbrickFiles)) {
+            throw new InstallationException(sprintf('Bundle "%s" hasn\'t objectbrick definition templates', $this->bundle->getName()));
         }
 
         try {
@@ -71,11 +80,11 @@ class Installer extends AbstractInstaller
                 if (
                     empty($definitionArray['id']) ||
                     ClassDefinition::getById($definitionArray['id']) !== null ||
-                    ClassDefinition::getByName($definitionFile['className']) !== null
+                    ClassDefinition::getByName($definitionFile['name']) !== null
                 ) {
                     throw new InstallationException(
                         sprintf(
-                            'Failed to install. The system already has a DataObject with "%s" name and ID "%s"',
+                            'Failed to install. The system already has a Class with "%s" name and ID "%s"',
                             ucfirst($definitionArray['id']),
                             $definitionArray['id']
                         )
@@ -83,23 +92,55 @@ class Installer extends AbstractInstaller
                 }
             }
 
+            foreach ($objectbrickFiles as $objectbrickFile) {
+                $objectbrickString = file_get_contents($this->objectbrickFullPath.$objectbrickFile['file']);
+
+                if (!$objectbrickString) {
+                    throw new InstallationException(sprintf('Failed to install. "%s" objectbrick file is invalid', $objectbrickFile));
+                }
+
+                $objectbrickArray = json_decode($objectbrickString, true);
+
+                unset($objectbrickString);
+
+                if (!is_array($objectbrickArray)) {
+                    throw new InstallationException(sprintf('Failed to install. "%s" objectbrick file has invalid stucture', $definitionFile));
+                }
+
+                $brickList = new \Pimcore\Model\DataObject\Objectbrick\Definition\Listing();
+                $brickList = $brickList->load();
+
+                foreach ($brickList as $item) {
+                    if (strtolower($objectbrickFile['name']) === strtolower($item->getKey())) {
+                        throw new \Exception(sprintf('Brick with %s name already exists (lower/upper cases may be different)', $objectbrickFile['name']));
+                    }
+                }
+            }
+
             $notCreated = [];
+
             foreach ($definitionFiles as $item) {
                 if (!$this->createCless($item)['status']) {
                     $notCreated[] = $this->createCless($item);
                 }
             }
 
+            sleep(5);
+            foreach ($objectbrickFiles as $item) {
+                if (!$this->createObjectbrick($item)['status']) {
+                    $notCreated[] = $this->createObjectbrick($item);
+                }
+            }
+
             if (count($notCreated)) {
                 throw new InstallationException(
                     sprintf(
-                        count($notCreated) > 1 ? 'Failed to create "%s" classes' : 'Failed to create "%s" class',
-                        implode(', ', array_column($notCreated, 'className'))
+                        count($notCreated) > 1 ? 'Failed to create "%s" classes or objectbricks' : 'Failed to create "%s" classe or objectbrick',
+                        implode(', ', array_column($notCreated, 'name'))
                     )
                 );
             }
 
-            PimcoreService::createWebsiteSetting();
         } catch (\Exception $e) {
             throw new InstallationException("Failed to install: ".$e);
         }
@@ -135,21 +176,21 @@ class Installer extends AbstractInstaller
         return $this->isInstalled();
     }
 
-    private function getDefinitionFiles(): array
+    private function getDefinitionFiles(string $path, string $type): array
     {
         $files = [];
 
         if (
-            count(glob($this->defenitionFullPath . '*')) &&
-            $handle = opendir($this->defenitionFullPath)
+            count(glob($path . '*')) &&
+            $handle = opendir($path)
         ) {
             while (false !== ($file = readdir($handle))) {
                 $parts = [];
-                if (is_file($this->defenitionFullPath . $file)) {
-                    if (1 === preg_match('/^class_(.*)_export\.json$/', basename($file), $parts)) {
+                if (is_file($path . $file)) {
+                    if (1 === preg_match('/^'.$type.'_(.*)_export\.json$/', basename($file), $parts)) {
                         $files[] = [
                             'file' => $parts[0],
-                            'className' => $parts[1],
+                            'name' => $parts[1],
                         ];
                     }
                 }
@@ -164,14 +205,30 @@ class Installer extends AbstractInstaller
     public function createCless(array $definitionFile): array
     {
         $class = new ClassDefinition();
-        $class->setName($definitionFile['className']);
-        $class->setId(strtolower($definitionFile['className']));
+        $class->setName($definitionFile['name']);
+        $class->setId(strtolower($definitionFile['name']));
 
         $data = file_get_contents($this->defenitionFullPath . $definitionFile['file']);
         $definitionFile['status'] = Service::importClassDefinitionFromJson($class, $data, false, true);
 
         return $definitionFile;
     }
+
+    public function createObjectbrick(array $objectbrickFile): array
+    {
+        $string = file_get_contents($this->objectbrickFullPath . $objectbrickFile['file']);
+        $data = json_decode($string, true);
+
+        $brickDef = new \Pimcore\Model\DataObject\Objectbrick\Definition();
+        $brickDef->setKey($objectbrickFile['name']);
+        $brickDef->setTitle($data['title']);
+        $brickDef->save();
+
+        $objectbrickFile['status'] = Service::importObjectBrickFromJson($brickDef, $string, false, true);
+
+        return $objectbrickFile;
+    }
+
 
     private function getDataportFiles(): array
     {
